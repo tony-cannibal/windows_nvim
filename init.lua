75f1db6eaa2ff7b4c5fdb4228696caaa6f2c83e1
tony-cannibal <91645702+tony-cannibal@users.noreply.github.com>
2023-03-01 16:14:01 -0600

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,22 @@
+require 'ing.options'
+require "ing.keys"
+require "ing.plugins"
+require "ing.colorscheme"
+require "ing.nvimtreesiter"
+require "ing.lualine"
+require "ing.bufferline"
+require "ing.mason"
+require "ing.lsp"
+require "ing.null-ls"
+require "ing.comment"
+require "ing.autopairs"
+require "ing.toggleterm"
+require "ing.scrollbar"
+require "ing.indentline"
+require "ing.telescope"
+require "ing.nvimtree"
+require "ing.colorizer"
+require "ing.alpha"
+
+
+
---
+++ lua/ing/lsp.lua
@@ -0,0 +1,122 @@
+local status_ok, lsp = pcall(require, "lsp-zero")
+if not status_ok then
+    return
+end
+
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+    return
+end
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    setup_servers_on_start = true,
+    set_lsp_keymaps = true,
+    configure_diagnostics = true,
+    cmp_capabilities = true,
+    manage_nvim_cmp = true,
+    call_servers = 'local',
+    underline = true,
+    everity_sort = true,
+    virtual_text = false,
+    signs = {
+        active = signs, -- show signs
+    },
+    sign_icons = {
+        error = '',
+        warn = '',
+        hint = '',
+        info = ''
+    },
+    float = {
+        focusable = true,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+    },
+})
+
+-- lsp.configure('sumneko_lua', {
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            }
+        }
+    }
+})
+
+local function lsp_keymaps(bufnr)
+    local opts = { noremap = true, silent = true }
+    local keymap = vim.api.nvim_buf_set_keymap
+    keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+    keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+    keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
+    keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
+    keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
+    keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
+    keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+end
+
+-- lsp.preset('recommended')
+lsp.setup()
+
+local null_opts = lsp.build_options('null-ls', {})
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+    on_attach = function(client, bufnr)
+        null_opts.on_attach(client, bufnr)
+        lsp_keymaps(bufnr)
+
+        local format_cmd = function(input)
+            vim.lsp.buf.format({
+                id = client.id,
+                timeout_ms = 5000,
+                async = input.bang,
+            })
+        end
+
+        local bufcmd = vim.api.nvim_buf_create_user_command
+        bufcmd(bufnr, 'NullFormat', format_cmd, {
+            bang = true,
+            range = true,
+            desc = 'Format using null-ls'
+        })
+        ---
+        -- you can add other stuff here....
+        ---
+    end,
+    debug = false,
+    sources = {
+        formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        formatting.black.with({ extra_args = { "--fast" } }),
+        formatting.stylua,
+        diagnostics.flake8
+    },
+})
+
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = false,
+    float = true,
+})
